6adf0fe9c7b7d6fe4c836b3742a23c842b0ae6eb
David Freites <davidfreites75@gmail.com>
2023-08-05 12:36:56 -0400

    primera entrega_mod

--- clases/main.js
+++ clases/main.js
@@ -3,23 +3,17 @@ for (let turno = 1; turno <= 3; turno++){
 
 let deuda = parseInt(prompt(`Ingrese el monto total de su deuda ${turno}`));
 let cuotas = parseInt(prompt("Ingrese la cantidad total de cuotas a pactar entre 2 y 36"));
-if (cuotas <= 36 && cuotas >= 2){
 function pagos(deuda,cuotas){
         return deuda / cuotas;
     }
-
-    }
+if (cuotas <= 36 && cuotas >= 2){
+        let resultado = pagos(deuda,cuotas);
+        alert (`El monto a pagar en cada cuota de la deuda ${turno} es de  ${resultado}`);
+}
 else{
-
-        alert("Error el numero de cuotas debe estar entre 2 y 36");
+        turno--;
+        alert("Error el numero de cuotas debe ser un numero entre 2 y 36 intente de nuevo");
         };
-
-    
-let resultado = pagos(deuda,cuotas);
-
-
-
-alert (`El monto a pagar en cada cuota de la deuda ${turno} es de  ${resultado}`);
 
 }
 
