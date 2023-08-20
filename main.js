fa3d17e5baa80748023757019ea27ffc5f877519
David Freites <davidfreites75@gmail.com>
2023-08-20 00:37:07 -0400

    segunda entrega

--- main.js
+++ main.js
@@ -1,21 +1,89 @@
-//pagos en cuotas sobre un monto determinado de 3 deudas
-for (let turno = 1; turno <= 3; turno++){
+///venta de Hamburguesas 
 
-    let deuda = parseInt(prompt(`Ingrese el monto total de su deuda ${turno}`));
-    let cuotas = parseInt(prompt("Ingrese la cantidad total de cuotas a pactar entre 2 y 36"));
-    function pagos(deuda,cuotas){
-            return deuda / cuotas;
+class Producto {
+///////el constructor recibe nombre y precio
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.vendido = false;
+    }
+    
+////////metodo para vender los productos
+    vender() {
+        this.vendido = true;
+    }
+}
+    ////////arreglo vacio que va a contener los productos
+const productos = [];
+    
+///////agregamos con el push objetos nuevos creados por la clase Producto al arreglo vacio
+productos.push(new Producto("la con queso", 10));
+productos.push(new Producto("la crispy", 12));
+productos.push(new Producto("la con queso doble", 14));
+productos.push(new Producto("la de la casa", 12));
+    
+//pedimos el nombre del producto a consultar
+let nombre = prompt("Ingrese el nombre del producto a consultar");
+const suma = (num1, num2) => num1 + num2;
+const resta = (num1, num2) => num1 - num2;
+const iva = (precioProducto) => precioProducto * 0.08;
+while (nombre != "ESC") {
+//declaramos la variable que contendrá el producto en caso de encontrarlo
+    let producto;
+    
+    //recorremos el arreglo verificando si alguno de los productos cumple con el nombre
+    for (const item of productos) {
+        if (item.nombre === nombre) {
+            producto = item;
         }
-    if (cuotas <= 36 && cuotas >= 2){
-            let resultado = pagos(deuda,cuotas);
-            alert (`El monto a pagar en cada cuota de la deuda ${turno} es de  ${resultado}`);
-    }
-    else{
-            turno--;
-            alert("Error el numero de cuotas debe ser un numero entre 2 y 36 intente de nuevo");
-            };
-    
     }
     
+    if (producto) {
+        let mensaje = `
+            Nombre: ${producto.nombre}
+            Precio: $${producto.precio} "+ tax - descuento"
+        `;
     
+        alert(mensaje);
+
+        if (producto.precio === 10){
     
+            let precioProducto = 10;
+            let descuento = 10 * 0.10;
+            // precioProducto + iva - descuento
+            let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)), descuento);
+            // = resta(suma(1000, iva(1000)), 100);
+            // = resta(suma(1000, 210), 100);
+            // = resta(1210, 100);
+            // = 1110
+            alert(`el precio final de su compra con el tax y el descuento es de $ ${nuevoPrecio}`);
+        }
+        if (producto.precio === 12){
+    
+            let precioProducto = 12;
+            let descuento = 12 * 0.10;
+            // precioProducto + iva - descuento
+            let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)), descuento);
+            // = resta(suma(1000, iva(1000)), 100);
+            // = resta(suma(1000, 210), 100);
+            // = resta(1210, 100);
+            // = 1110
+            alert(`el precio final de su compra con el tax y el descuento es de $ ${nuevoPrecio}`);
+        }
+        if (producto.precio === 14){
+    
+            let precioProducto = 14;
+            let descuento = 14 * 0.10;
+            // precioProducto + iva - descuento
+            let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)), descuento);
+            // = resta(suma(1000, iva(1000)), 100);
+            // = resta(suma(1000, 210), 100);
+            // = resta(1210, 100);
+            // = 1110
+            alert(`el precio final de su compra con el tax y el descuento es de $ ${nuevoPrecio}`);
+        }
+    } else {
+        alert("El producto no se encuentra disponible");
+    }
+nombre = prompt("Ingrese el nombre del productoa  consultar");
+}
